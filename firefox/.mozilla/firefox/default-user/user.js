4c8886428f1d73c004eebf5bb2d5b16289ab4408
Kjetil Rambjørg <rakj0804@gmail.com>
2024-12-17 21:45:01 +0100

    firefox config wip

---
+++ firefox/.mozilla/firefox/default-user/user.js
@@ -0,0 +1,5 @@
+user_pref("browser.aboutConfig.showWarning", false);
+user_pref("browser.tabs.inTitlebar", 0);
+user_pref("browser.tabs.hoverPreview.enabled", false);
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
+
