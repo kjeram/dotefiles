5d295e0cdc56b9d9437144a9054856ab4d0f5c7e
Kjetil Rambjørg <rakj0804@gmail.com>
2025-01-08 12:26:54 +0100

    firefox profile config

---
+++ firefox/.mozilla/firefox/default-user/user.js
@@ -0,0 +1,32 @@
+user_pref("accessibility.force_disabled", true);
+
+user_pref("browser.aboutConfig.showWarning", false);
+user_pref("browser.formfill.enable", false);
+user_pref("browser.newtabpage.activity-stream.feeds.topsites", false);
+user_pref("browser.newtabpage.activity-stream.section.highlights.includeBookmarks", false);
+user_pref("browser.newtabpage.activity-stream.section.highlights.includeDownloads", false);
+user_pref("browser.newtabpage.activity-stream.section.highlights.includeVisited", false);
+user_pref("browser.newtabpage.activity-stream.showSearch", false);
+user_pref("browser.tabs.inTitlebar", 0);
+user_pref("browser.theme.toolbar-theme", 0);
+user_pref("browser.tabs.hoverPreview.enabled", false);
+user_pref("browser.toolbars.bookmarks.visibility", "never");
+user_pref("layout.css.prefers-color-scheme.content-override", 0);
+
+user_pref("browser.uiCustomization.state", {"placements":{"widget-overflow-fixed-list":[],"unified-extensions-area":[],"nav-bar":["back-button","forward-button","stop-reload-button","urlbar-container","save-to-pocket-button","downloads-button","unified-extensions-button"],"toolbar-menubar":["menubar-items"],"TabsToolbar":["firefox-view-button","tabbrowser-tabs","new-tab-button","alltabs-button"],"vertical-tabs":[],"PersonalToolbar":["personal-bookmarks"]},"seen":["developer-button"],"dirtyAreaCache":["nav-bar","vertical-tabs"],"currentVersion":20,"newElementCount":3};
+user_pref("browser.urlbar.suggest.topsites", false);
+
+user_pref("devtools.chrome.enabled", true);
+user_pref("devtools.debugger.remote-enabled", true);
+user_pref("devtools.everOpened", true);
+user_pref("devtools.theme", "dark");
+user_pref("devtools.toolbox.host", window);
+
+user_pref("extensions.pocket.enabled", false);
+user_pref("extensions.screenshots.disabled", true);
+
+user_pref("general.smoothScroll", false);
+
+user_pref("geo.enabled", false);
+
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
