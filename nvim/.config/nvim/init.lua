c69452726a48d94213231dc270eb1c580fae120e
Kjetil Rambjørg <rakj0804@gmail.com>
2025-02-04 21:07:42 +0100

    Update nvim (WIP)

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,48 +1,15 @@
--------------------------
--- Bootstrap lazy.nvim --
--------------------------
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-  if vim.v.shell_error ~= 0 then
-    vim.api.nvim_echo({
-      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out, "WarningMsg" },
-      { "\nPress any key to exit..." },
-    }, true, {})
-    vim.fn.getchar()
-    os.exit(1)
-  end
-end
-vim.opt.rtp:prepend(lazypath)
+require("config.lazy")
 
--- Make sure to setup `mapleader` and `maplocalleader` before
--- loading lazy.nvim so that mappings are correct.
--- This is also a good place to setup other settings (vim.opt)
-vim.g.mapleader = " "
-vim.g.maplocalleader = "\\"
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
 
--- Setup lazy.nvim
-require("lazy").setup({
-  spec = {
-    -- add your plugins here
-    { "catppuccin/nvim", name = "catppuccin", priority = 1000 }
-  },
-  -- Configure any other settings here. See the documentation for more details.
-  -- colorscheme that will be used when installing plugins.
-  install = { colorscheme = { "catppuccin" } },
-  -- automatically check for plugin updates
-  checker = { enabled = true },
-})
+vim.opt.number = true
 
------------
--- Theme --
------------
-vim.cmd.colorscheme "catppuccin-mocha"
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set("n", "<leader><S-j>", vim.cmd.tabp)
+vim.keymap.set("n", "<leader><S-k>", vim.cmd.tabn)
+vim.keymap.set("n", "<leader>ct", vim.cmd.tabnew)
 
-----------
--- Misc --
-----------
-vim.cmd("highlight Normal guibg=#00000000")
-vim.cmd("se number")
+-- Make copy-paste less of a hassle
+vim.opt.clipboard = 'unnamedplus'
+vim.keymap.set("v", "<C-c>", 'y')
