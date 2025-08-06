d4d1454b727614cffd8f384d5b1b1f774b9d6d7f
Kjetil Rambjørg <kjetil.rambjorg@mattilsynet.no>
2025-08-06 15:10:52 +0200

    nvim: Improve blink

--- nvim/.config/nvim/lua/plugins/configs/blink.lua
+++ nvim/.config/nvim/lua/plugins/configs/blink.lua
@@ -1,8 +1,16 @@
 return {
-  keymap = { preset = 'enter' },
-  completion = { documentation = { auto_show = true } },
-  sources = {
-    default = { 'lsp', 'path', 'snippets', 'buffer' },
+  keymap = {
+    preset = 'enter',
+    ['<C-k>'] = { 'select_prev', 'fallback' },
+    ['<C-j>'] = { 'select_next', 'fallback' },
   },
-  fuzzy = { implementation = "lua" }
+  fuzzy = { implementation = "lua" },
+  completion = {
+    ghost_text = { enabled = true },
+    documentation = {
+      auto_show = true,
+      auto_show_delay_ms = 200,
+      window = { border = "rounded" },
+    },
+  },
 }
