0b9ee9fde5e9fd6875266c27d0f1bb693c520fec
Kjetil Rambjørg <kjetil.rambjorg@mattilsynet.no>
2025-09-30 12:09:45 +0200

    nvim: Add keybind to delete buffers within telescope

--- nvim/.config/nvim/lua/plugins/configs/telescope.lua
+++ nvim/.config/nvim/lua/plugins/configs/telescope.lua
@@ -1,3 +1,15 @@
 require 'telescope'.setup {
   pcall(require('telescope').load_extension, 'fzf'),
+  pickers = {
+    buffers = {
+      mappings = {
+        i = {
+          ["<C-q>"] = require('telescope.actions').delete_buffer,
+        },
+        n = {
+          ["<C-q>"] = require('telescope.actions').delete_buffer,
+        }
+      }
+    }
+  }
 }
