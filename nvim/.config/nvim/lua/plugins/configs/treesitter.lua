c122d6c467728fbf2f30c3c3acac8ba0cf55ede6
Kjetil Rambjørg <kjetil.rambjorg@mattilsynet.no>
2026-04-07 08:29:13 +0200

    nvim: Update treesitter

--- nvim/.config/nvim/lua/plugins/configs/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/configs/treesitter.lua
@@ -1,8 +1,11 @@
-require 'nvim-treesitter.configs'.setup {
-  ensure_installed = { "lua", "vim", "vimdoc", "python", "go", "sql" },
-  auto_install = true,
-  highlight = {
-    enable = true,
-  },
-  indent = { enable = true, },
-}
+require 'nvim-treesitter'.install { "lua", "vim", "vimdoc", "python", "go", "sql" }
+
+vim.api.nvim_create_autocmd('FileType', {
+    pattern = { "lua", "vim", "vimdoc", "python", "go", "sql" },
+    callback = function()
+      -- syntax highlighting, provided by Neovim
+      vim.treesitter.start()
+      -- indentation, provided by nvim-treesitter
+      vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+    end,
+  })
--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -58,7 +58,6 @@ return {
 
   {
     "nvim-treesitter/nvim-treesitter",
-    branch = 'master',
     build = ":TSUpdate",
     lazy = false,
     config = function() require "plugins.configs.treesitter" end,
